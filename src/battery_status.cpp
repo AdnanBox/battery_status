7afc456779465ccacaace612824ea84d5464cb6d
ipa-fog-ab <15811079+ipa-fog-ab@users.noreply.github.com>
2018-09-11 13:48:16 +0200

    Add files via upload
    integrated colorRGBA topic in battery_status

--- src/battery_status.cpp
+++ src/battery_status.cpp
@@ -1,11 +1,13 @@
 #include <ros/ros.h>
 #include "std_msgs/Float32.h"
+#include "std_msgs/ColorRGBA.h"
 #include <wiringPi.h>
 #include <wiringSerial.h> 
 #include <iostream>
 
 #define PORT_ADC1 0 //ADC.AIN0
-static int adcValue = 0;
+std_msgs::ColorRGBA color;
+ros::Publisher pub_color;
 
 float battery_voltage(float volt)
 {
@@ -15,7 +17,8 @@ float battery_voltage(float volt)
 
 void statusCallback(const std_msgs::Float32::ConstPtr& msg)
 {
-if (msg->data <= 4096 && msg->data>4050)
+
+    if (msg->data <= 4096 && msg->data>4050)
     {
         std::cout<<"100% :"<<battery_voltage(msg->data)<<"V"<<std::endl;
     }
@@ -41,24 +44,58 @@ if (msg->data <= 4096 && msg->data>4050)
     }
 }
 
+
+void colorCallback(const std_msgs::Float32::ConstPtr& msg)
+{   
+    if (msg->data <= 4096 && msg->data > 3000)
+    {
+        color.r=0;
+        color.g=1;
+        color.b=0;
+        color.a=1;
+    }
+    else if (msg->data <= 3000 && msg->data > 2000)
+    {
+        color.r=1;
+        color.g=1;
+        color.b=0;
+        color.a=1;
+    }
+    else if (msg->data <= 2000 && msg->data > 1000)
+    {
+        color.r=1;
+        color.g=0;
+        color.b=0;
+        color.a=1;
+    }
+    pub_color.publish(color);
+}
+
+
 int main(int argc, char *argv[])
 {
     
     wiringPiSetup();
-    ros::init(argc, argv, "publisher");
+    ros::init(argc, argv, "Battery_Status");
     ros::NodeHandle n;
-    ros::Publisher pub = n.advertise<std_msgs::Float32>("status",1000);
-    ros::Subscriber sub = n.subscribe("status", 1000, statusCallback);
+    ros::Publisher pub_status = n.advertise<std_msgs::Float32>("status",100);
+    ros::Subscriber sub_status = n.subscribe("status", 100, statusCallback);
+    ros::Subscriber sub_color = n.subscribe("status", 100, colorCallback);
+
+
+    pub_color = n.advertise<std_msgs::ColorRGBA>("color",100);
+  
+    std_msgs::Float32 analog_voltage;
+
     ros::Rate rate(1);
    
     while(ros::ok())    
     {
 
-        std_msgs::Float32 analog_voltage;
-        adcValue = analogRead(PORT_ADC1);
-        analog_voltage.data = adcValue;
-        pub.publish(analog_voltage);
-        ros::Subscriber sub = n.subscribe("status", 1000, statusCallback);
+
+        analog_voltage.data = analogRead(PORT_ADC1);
+        pub_status.publish(analog_voltage);
+        
         ros::spinOnce();
         rate.sleep();
 
---
+++ src/led_glow.cpp
@@ -0,0 +1,40 @@
+#include <ros/ros.h>
+#include "std_msgs/Float32.h"
+#include "std_msgs/ColorRGBA.h"
+#include "wiringPi.h"
+#include "wiringSerial.h"
+
+#define RED_LED 0 //pin 11
+#define GREEN_LED 2 //pin 13 
+#define BLUE_LED 3 //pin 15
+
+void setup()
+  {
+    wiringPiSetup();
+    pinMode(RED_LED, OUTPUT);
+    pinMode(GREEN_LED, OUTPUT);
+    pinMode(BLUE_LED, OUTPUT);
+    digitalWrite(RED_LED, HIGH);
+    digitalWrite(GREEN_LED, HIGH);
+    digitalWrite(BLUE_LED, HIGH);
+  }
+
+void ledCallback(const std_msgs::ColorRGBA::ConstPtr& msg)
+  {
+    digitalWrite(RED_LED, HIGH*msg->r);
+    digitalWrite(GREEN_LED, HIGH*msg->g);
+    digitalWrite(BLUE_LED, HIGH*msg->b);
+    ROS_INFO_STREAM("test val red  :  "<<HIGH*msg->r);
+    ROS_INFO_STREAM("test val green  :  "<<HIGH*msg->g);
+    ROS_INFO_STREAM("test val blue  :  "<<HIGH*msg->b);    
+  }
+
+int main(int argc, char *argv[])
+  {
+    setup();
+    ros::init(argc, argv, "LED_indicator");
+    ros::NodeHandle n;
+    ros::Subscriber sub_led = n.subscribe("color", 100, ledCallback);
+    ros::spin();
+    return 0;
+  }
